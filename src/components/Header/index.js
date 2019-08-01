7027623ac3e5b83176a934fe15b23afa3bb466f6
cterrasid <clarette.terrasi@gmail.com>
2019-08-02 01:31:16 +0200

    Update Header component styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './styles.scss';
 
 const Header = props => {
   const { title, logo } = props;
 
   return (
-    <header className="header__container">
-      <img className="header__logo" src={logo.url} alt={logo.alt} />
-      <h1 className="header__title">{title}</h1>
-    </header>
+    <Fragment>
+      <header className="header__container">
+        <img className="header__logo" src={logo.url} alt={logo.alt} />
+        <h1 className="header__title">{title}</h1>
+      </header>
+      <svg
+        id="bigTriangleColor"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        viewBox="0 0 100 102"
+        preserveAspectRatio="none"
+      >
+        <path d="M0 0 L50 100 L100 0 Z" />
+      </svg>
+    </Fragment>
   );
 };
 
